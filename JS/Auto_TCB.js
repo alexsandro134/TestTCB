d08846c954b1ff33e5c35062be0f02478a471cc2
Tai Le <alexsandro134@gmail.com>
2018-06-15 13:25:05 +0700

    Update format date

--- JS/Auto_TCB.js
+++ JS/Auto_TCB.js
@@ -1,9 +1,9 @@
 function addMonth(inputDate, x) {
     if (isDate(inputDate)) {
         var day, month, year;
-        month = inputDate.substring(0, 2) - 1;
-        day = inputDate.substring(2, 4) - 0;
-        year = inputDate.substring(4, 8) - 0;
+        month = inputDate.substring(4, 6) - 1;
+        day = inputDate.substring(6, 8) - 0;
+        year = inputDate.substring(0, 4) - 0;
         var currentDate = new Date(year, month, day);
         currentDate.setMonth(currentDate.getMonth() + parseInt(x));
         document.getElementById('resultAddMonth').innerHTML = currentDate.toLocaleDateString('en-US');
@@ -14,13 +14,13 @@ function addMonth(inputDate, x) {
 function monthDiff(startInput, closeInput) {
     if (isDate(startInput) && isDate(closeInput)) {
         var startDate, startDay, startMonth, startYear, closeDate, closeMonth, closeDay, closeYear;
-        startMonth = startInput.substring(0, 2) - 1;
-        startDay = startInput.substring(2, 4) - 0;
-        startYear = startInput.substring(4, 8) - 0;
+        startMonth = startInput.substring(4, 6) - 1;
+        startDay = startInput.substring(6, 8) - 0;
+        startYear = startInput.substring(0, 4) - 0;
         startDate = new Date(startYear, startMonth, startDay);
-        closeMonth = closeInput.substring(0, 2) - 1;
-        closeDay = closeInput.substring(2, 4) - 0;
-        closeYear = closeInput.substring(4, 8) - 0;
+        closeMonth = closeInput.substring(4, 6) - 1;
+        closeDay = closeInput.substring(6, 8) - 0;
+        closeYear = closeInput.substring(0, 4) - 0;
         closeDate = new Date(closeYear, closeMonth, closeDay);
         var monthDifferent = closeDate.getMonth() - startDate.getMonth() + (12 * (closeDate.getFullYear() - startDate.getFullYear()));
         document.getElementById('result').innerHTML = monthDifferent + " month(s) between";
@@ -32,13 +32,13 @@ function dayDiff(startInput, closeInput) {
     if (isDate(startInput) && isDate(closeInput)) {
         var msInADay = 24 * 60 * 60 * 1000;
         var startDate, startDay, startMonth, startYear, closeDate, closeMonth, closeDay, closeYear;
-        startMonth = startInput.substring(0, 2) - 1;
-        startDay = startInput.substring(2, 4) - 0;
-        startYear = startInput.substring(4, 8) - 0;
+        startMonth = startInput.substring(4, 6) - 1;
+        startDay = startInput.substring(6, 8) - 0;
+        startYear = startInput.substring(0, 4) - 0;
         startDate = new Date(startYear, startMonth, startDay);
-        closeMonth = closeInput.substring(0, 2) - 1;
-        closeDay = closeInput.substring(2, 4) - 0;
-        closeYear = closeInput.substring(4, 8) - 0;
+        closeMonth = closeInput.substring(4, 6) - 1;
+        closeDay = closeInput.substring(6, 8) - 0;
+        closeYear = closeInput.substring(0, 4) - 0;
         closeDate = new Date(closeYear, closeMonth, closeDay);
         daysBetween = (closeDate.getTime() - startDate.getTime()) / msInADay;
         document.getElementById('result').innerHTML = daysBetween + " day(s) between";
@@ -55,9 +55,9 @@ function isDate(inputDate) {
     if (inputDate.length !== 8) {
         return false;
     }
-    month = inputDate.substring(0, 2) - 1;
-    day = inputDate.substring(2, 4) - 0;
-    year = inputDate.substring(4, 8) - 0;
+    month = inputDate.substring(4, 6) - 1;
+    day = inputDate.substring(6, 8) - 0;
+    year = inputDate.substring(0, 4) - 0;
     if (year < 1000 || year > 3000) {
         return false;
     }
